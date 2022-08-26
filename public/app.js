dbdc2aa22fc1237102c3e2ef07c1e04ca2f50437
thepeekay <panagiotis@kefalidis.me>
2022-08-25 17:25:25 -0700

    Fixing and solving

--- public/app.js
+++ public/app.js
@@ -1,19 +1,45 @@
-// create map
-
+// Create map:                                                       
+// Create map:                                                       
+var map = L.map('map').setView([51.505, -0.09], 13);
 
 // add openstreetmap tiles
-
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
 
 // create and main add geolocation marker
+// Create and add a geolocation marker:
+const marker = L.marker([48.87007, 2.346453])
+marker.addTo(map).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
 
 
 // draw the 2nd arrondissement
+var polygon = L.polygon([
+    [48.863368120198004, 2.3509079846928516],
+    [48.86933262048345, 2.3542531602919805],
+    [48.87199261164275, 2.3400569901592183],
+    [48.86993336274516, 2.3280142476578813],
+    [48.86834104280146, 2.330308418109664]
+]).addTo(map);
 
+var redPin = L.icon({
+    iconUrl: 'assets/red-pin.png',
+    iconSize: [38, 38],
+    iconAnchor: [19, 38],
+});
 
 // create red pin marker
-
+const rS = L.marker([48.866200610611926, 2.352236247419453], { icon: redPin }).bindPopup('RÃ©aumur-SÃ©bastopol')
 
 // metro station markers
+const sSD = L.marker([48.869531786321566, 2.3528590208055196]).bindPopup('Strasbourg-Saint-Denis')
+const sentier = L.marker([48.8673721067762, 2.347107922912739]).bindPopup('Sentier')
+const bourse = L.marker([48.86868503971672, 2.3412285142058167]).bindPopup('Bourse')
+const qS = L.marker([48.869560129483226, 2.3358638645569543]).bindPopup('Quatre Septembre')
+const gB = L.marker([48.871282159004856, 2.3434818588892714]).bindPopup('Grands Boulevards')
+
+const stations = L.layerGroup([rS, sSD, sentier, bourse, qS, gB]).addTo(map)
 
 
 
